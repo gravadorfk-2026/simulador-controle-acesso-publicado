bd371ae01d9bbe7dd4595b4e84843854f6e6b2d8
gravadorfk-2026 <gravadorfk@gmail.com>
2026-09-16 16:21:37 +0000

    Atualiza simulador publicado

--- assets/selection.fragment-aBOWpD2H.js
+++ assets/selection.fragment-aBOWpD2H.js
@@ -1,4 +1,4 @@
-import{S as i}from"./index-CMZSXHlF.js";const e="selectionPixelShader",f=`#ifdef INSTANCES
+import{S as i}from"./index-DbKvvd8g.js";const e="selectionPixelShader",f=`#ifdef INSTANCES
 flat varying vSelectionId: f32;
 #else
 uniform selectionId: f32;
--- assets/selection.vertex-DTr7PjP4.js
+++ assets/selection.vertex-DTr7PjP4.js
@@ -1,4 +1,4 @@
-import{S as i}from"./index-CMZSXHlF.js";const e="selectionVertexShader",t=`attribute vec3 position;
+import{S as i}from"./index-DbKvvd8g.js";const e="selectionVertexShader",t=`attribute vec3 position;
 #ifdef INSTANCES
 attribute float instanceSelectionId;
 #endif
