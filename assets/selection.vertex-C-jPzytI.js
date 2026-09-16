18285c0351a7ff9edbda333164caa55a26174521
gravadorfk-2026 <gravadorfk@gmail.com>
2026-09-16 17:29:19 +0000

    Atualiza simulador publicado

--- assets/selection.vertex-C-jPzytI.js
+++ assets/selection.vertex-C-jPzytI.js
@@ -1,4 +1,4 @@
-import{S as t}from"./index-C-F6ZSEl.js";const e="selectionVertexShader",i=`attribute position: vec3f;
+import{S as t}from"./index-CDfmVG8c.js";const e="selectionVertexShader",i=`attribute position: vec3f;
 #ifdef INSTANCES
 attribute instanceSelectionId: f32;
 #endif
--- assets/selection.vertex-CdIOiIQj.js
+++ assets/selection.vertex-CdIOiIQj.js
@@ -1,4 +1,4 @@
-import{S as i}from"./index-C-F6ZSEl.js";const e="selectionVertexShader",t=`attribute vec3 position;
+import{S as i}from"./index-CDfmVG8c.js";const e="selectionVertexShader",t=`attribute vec3 position;
 #ifdef INSTANCES
 attribute float instanceSelectionId;
 #endif
