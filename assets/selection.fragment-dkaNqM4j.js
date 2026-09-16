957943ea5192a0d3a761d614008c895761451a60
gravadorfk-2026 <gravadorfk@gmail.com>
2026-09-16 15:16:40 +0000

    Atualiza simulador para versão 0.2.6

--- assets/selection.fragment-dkaNqM4j.js
+++ assets/selection.fragment-dkaNqM4j.js
@@ -1,4 +1,4 @@
-import{S as i}from"./index-8MafkwZb.js";const e="selectionPixelShader",d=`#ifdef INSTANCES
+import{S as i}from"./index-CMZSXHlF.js";const e="selectionPixelShader",d=`#ifdef INSTANCES
 flat varying float vSelectionId;
 #else
 uniform float selectionId;
--- assets/selection.vertex-CXw6_4S0.js
+++ assets/selection.vertex-CXw6_4S0.js
@@ -1,4 +1,4 @@
-import{S as t}from"./index-8MafkwZb.js";const e="selectionVertexShader",i=`attribute position: vec3f;
+import{S as t}from"./index-CMZSXHlF.js";const e="selectionVertexShader",i=`attribute position: vec3f;
 #ifdef INSTANCES
 attribute instanceSelectionId: f32;
 #endif
