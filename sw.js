435048a51afa71490128815258245246fd36f1ec
gravadorfk-2026 <gravadorfk@gmail.com>
2026-09-17 13:07:53 +0000

    Atualiza simulador publicado

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "simulador-controle-acesso-v0.2.92";
+const CACHE = "simulador-controle-acesso-v0.3.0";
 const ESSENTIAL = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => {
